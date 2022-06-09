3a7626d61f72e85e14547be88c5b001508ed3e2c
Lucas Balieiro <lucasbalieirom@hotmail.com>
2022-06-09 16:05:11 -0400

    chore: add jst scripts

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
     collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
     coverageDirectory: 'coverage',
     testEnvironment: 'node',
-    "collectCoverage": true,
     transform: {
         '.+\\.ts$': 'ts-jest',
     }
